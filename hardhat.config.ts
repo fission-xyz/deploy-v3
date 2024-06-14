99f6fe2338d7616f9b96bd3089a54dac61b96618
Kyryl Riabov <kyryl.ryabov@gmail.com>
2024-06-14 13:51:49 +0300

    Added support for polygon network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,11 +20,27 @@ const config: HardhatUserConfig = {
     },
     localhost: {
       url: "http://127.0.0.1:8545",
-      gasMultiplier: 1.15,
+      gasMultiplier: 1.2,
     },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts,
+      gasMultiplier: 1.2,
+    },
+    amoy: {
+      url: `https://polygon-amoy.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      gasMultiplier: 1.2,
+    },
+    ethereum: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      gasMultiplier: 1.2,
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      gasMultiplier: 1.2,
     },
   },
 };
